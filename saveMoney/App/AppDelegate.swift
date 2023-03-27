dfc70555c06135db21f7299917a774d8013c8aaa
Дмитрий Пантелеев <d.panteleev@profsoft.pro>
2023-03-27 13:06:33 +0400

    addd AppDeleagte

---
+++ saveMoney/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  saveMoney
+//
+//  Created by Дмитрий Пантелеев on 27.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
