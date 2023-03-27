3b6f57a5bf0f8543a95bf572b697a94e99f639d3
Дмитрий Пантелеев <d.panteleev@profsoft.pro>
2023-03-27 10:45:48 +0400

    Initial Commit

---
+++ saveMoney/saveMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  saveMoneyApp.swift
+//  saveMoney
+//
+//  Created by Дмитрий Пантелеев on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct saveMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
